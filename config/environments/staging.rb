d63c7dac03a098deaf98ccb69a022623340e6806
winstonferguson <winston@poshock.com>
2023-04-26 18:31:52 +0100

    staging set up

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "http://staging.dryer.dev"
+  config.asset_host = "http://185.27.21.20"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
