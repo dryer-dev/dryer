0ae2707db3fb73c0c739c5ff4ffe75f32a298305
winstonferguson <winston@poshock.com>
2023-04-26 17:16:54 +0100

    staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << 'staging.dryer.dev'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -32,7 +33,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
+  config.asset_host = "https://staging.dryer.dev"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
