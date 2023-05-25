9a008fc54a890d2ec3a21b40ea296531da12c649
winstonferguson <winston@poshock.com>
2023-05-25 16:45:50 +0100

    nestings

--- app/models/nesting.rb
+++ app/models/nesting.rb
@@ -8,13 +8,4 @@ class Nesting < ApplicationRecord
 
   belongs_to :childable,
              polymorphic: true
-
-  validates_associated :handle_children
-
-  def handle_children
-    puts '__________________________________________________________________NESTIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    puts parentable.inspect
-    puts childable.inspect
-    puts attributes.inspect
-  end
 end
